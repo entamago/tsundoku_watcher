2b3be4b6d2a3b8cd8126bd143f72d37493ee4bb1
entamago <81839879+entamago@users.noreply.github.com>
2021-07-31 15:55:20 +0900

    ユーザー編集機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :nickname, length: {maximum: 10, message: "10文字以内で入力してください"}
+    validates :purpose, length: {maximum: 30, message: "30文字以内で入力してください"}
+  end
+
   has_many :genres
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :genres, only: :index
-  root to: 'genres#index'
+  resources :genres, only: [:index, :new, :create]
+  resources :users, only: [:show, :edit, :update]
+  root to: 'users#show'
 end
