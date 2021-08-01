63c4a1a65e307899a3cb7c111536e10b8ad0f87d
entamago <81839879+entamago@users.noreply.github.com>
2021-08-02 00:57:15 +0900

    ジャンルの追加・編集・削除機能

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -12,7 +12,37 @@ class GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    
+    if @genre.save
+      redirect_to genres_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to genres_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    if @genre.destroy
+      redirect_to genres_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
   end
 
   private
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,8 @@
 class Genre < ApplicationRecord
   belongs_to :user
 
-
+  with_options presence: true do
+    validates :genre
+    validates :genre_info
+  end
 end
