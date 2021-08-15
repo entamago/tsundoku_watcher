ab43ccadad17d73b817098a5a32d1bda97d16dba
entamago <81839879+entamago@users.noreply.github.com>
2021-08-15 12:55:26 +0900

    Faker導入とuserモデルのテストコード記載

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Internet.username}
-    purpose               {Faker::Lorem.characters(number: 15)}
+    nickname              {Faker::Lorem.characters(number: 10)}
+    purpose               {Faker::Lorem.characters(number: 13)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
